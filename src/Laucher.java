34eccd8288191c70f4cc9c045a3cdde6e4bec531
tanya haliche <tanya.haliche@epita.fr>
2022-02-11 16:17:40 +0100

    p3

--- src/Laucher.java
+++ src/Laucher.java
@@ -4,12 +4,14 @@ import java.util.Scanner;
 public class Launcher {
     public static void main(String[] args) {
         System.out.println("Bienvenu");
-        Scanner scanner= new Scanner(System.in);
         System.out.println("quelque chose");
-        String rep = scanner.nextLine();
-        if (!rep.equals("quit")) {
+        Scanner scanner= new Scanner(System.in);
+        do {
+            String instruction = scanner.next();
+            if ("quit".equals(instruction))
+                break;
             System.out.println("Unknown command");
-        }
-
+        } while (true);        
     }
+
 }
