725ad4b05d9442c4ca73eb844f8bbbf5424a4741
tanya haliche <tanya.haliche@epita.fr>
2022-02-11 16:21:48 +0100

    p4

--- src/Laucher.java
+++ src/Laucher.java
@@ -2,6 +2,18 @@ import java.util.Scanner;
 
 
 public class Launcher {
+    public static int fibonacci(int n){
+        int u1 = 0;
+        int u2 = 1;
+        int somme = 0;
+        for (int i = 0; i < n; i++)
+        {
+            somme = u1 + u2;
+            u1 = u2;
+            u2 = somme;
+        }
+        return somme;
+    }
     public static void main(String[] args) {
         System.out.println("Bienvenu");
         System.out.println("quelque chose");
@@ -9,8 +21,17 @@ public class Launcher {
         do {
             String instruction = scanner.next();
             if ("quit".equals(instruction))
-                break;
-            System.out.println("Unknown command");
+                breaki;
+            else if (rep.equals("fibo")){
+                System.out.println("Entrer un entier n");
+                int n = scanner.nextInt();
+                scanner.nextLine();
+                System.out.println(fibonacci(n));
+            }
+            else 
+            {
+                System.out.println("Unknown command");
+            }
         } while (true);        
     }
 
