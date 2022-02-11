a3d705527850d7b93e9252ff8fc06c8f9782d1fd
tanya haliche <tanya.haliche@epita.fr>
2022-02-11 18:19:58 +0100

    p5

--- src/Laucher.java
+++ src/Laucher.java
@@ -1,4 +1,10 @@
 import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+
 
 
 public class Launcher {
@@ -14,6 +20,32 @@ public class Launcher {
         }
         return somme;
     }
+    public static List<String> word_freq(String str){
+        str = str.toLowerCase().replaceAll("[.!?\\-'\"\n]", " ");
+
+        Map<String, Integer> wofreq = new HashMap<>();
+
+        for (var word : str.split(" ")){
+            if (word.isBlank()) continue;
+
+            wofreq.putIfAbsent(word,0);
+            wofreq.put(word, wofreq.get(word) + 1);
+        }
+
+        List<String> mots = new ArrayList<>();
+
+        while (mots.size() < 3 && wofreq.keySet().size() > 0){
+            int max = Collections.max(wofreq.values());
+            var keys = wofreq.keySet().stream().filter(k -> wofreq.get(k) == max).collect(Collectors.toList());
+            var last = keys.get(keys.size() - 1);
+            mots.add(last);
+            wofreq.remove(last);
+        }
+        return mots;
+
+    }
+
+
     public static void main(String[] args) {
         System.out.println("Bienvenu");
         System.out.println("quelque chose");
@@ -21,13 +53,22 @@ public class Launcher {
         do {
             String instruction = scanner.next();
             if ("quit".equals(instruction))
-                breaki;
-            else if (rep.equals("fibo")){
-                System.out.println("Entrer un entier n");
-                int n = scanner.nextInt();
-                scanner.nextLine();
-                System.out.println(fibonacci(n));
-            }
+                break;
+            else if (rep.equals("freq")){
+                System.out.println("Quel est le chemin du fichier ?");
+                String chemin = scanner.nextLine();
+                Path file = Paths.get(chemin);
+                String content;
+                try {
+                    content = Files.readString(file);
+                }
+                catch (Exception e) {
+                    System.err.println("Unreadable file: " + e.getMessage());
+                    break;
+                }
+                List<String> mots = word_freq(content);
+                System.out.println(String.join(" ",mots));
+            }    
             else 
             {
                 System.out.println("Unknown command");
